6c9e9180ddcd3f895227f366b376b644cbdff4e1
Lius <liushuangmail@126.com>
2017-07-07 08:01:16 +0800

    add Role model

--- pretty-blog/app/models.py
+++ pretty-blog/app/models.py
@@ -2,16 +2,66 @@
 from . import db, login_manager
 from flask_login import UserMixin, AnonymousUserMixin
 from datetime import datetime
+from werkzeug.security import generate_password_hash, check_password_hash        ###
+
+
+class Permission:
+    FOLLOW = 0x01  #没有逗号
+    COMMENT = 0x02
+    WRITE = 0x04
+    MODERATE = 0x08
+    ADMIN = 0x80
+
+class Role(db.Model):
+    __tablename__ = "roles"
+    id = db.Column(db.Integer, primary_key=True)
+    permissions = db.Column(db.Integer)
+    users = db.relationship("User", backref="role", lazy="dynamic")
+    name = db.Column(db.String(64), unique=True)##unique
+    default = db.Column(db.Boolean, default=False)
+
+    @staticmethod
+    def create_roles(): #not have self
+        roles = {
+            "User":(Permission.WRITE |
+                    Permission.COMMENT |
+                    Permission.FOLLOW, True),
+            "Admin":(0xff, False),
+            "Moderator":(Permission.WRITE |
+                         Permission.COMMENT |
+                         Permission.FOLLOW |
+                         Permission.MODERATE, False)
+        }
+        for r in roles:
+            role = Role.query.filter_by(name=r).first()
+            if role is None:
+                role = Role(name=r, permissions=roles[r][0], default=roles[r][1])
+                db.session.add(role)
+        db.session.commit()
+
 
 class User(UserMixin, db.Model):
     __tablename__ = "users"
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), unique=True)
-    password = db.Column(db.String(64))
+    password_hash = db.Column(db.String(64))
     location = db.Column(db.String(64))
     about_me = db.Column(db.Text)
     posts = db.relationship("Post", backref="author", lazy="dynamic")
     comments = db.relationship("Comment", backref="author", lazy="dynamic")
+    role_id = db.Column(db.Integer, db.ForeignKey("roles.id"))
+
+    @property
+    def password(self):
+        raise AttributeError("Password can not be seen")
+
+    @password.setter
+    def password(self, password):
+        self.password_hash = generate_password_hash(password)  #self.password_hash
+
+    def verify_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
 
 class Post(db.Model):
     __tablename__ = "posts"
--- pretty-blog/manager.py
+++ pretty-blog/manager.py
@@ -3,6 +3,8 @@
 from flask_script import Manager, Shell
 from app import create_app, db
 from flask_migrate import Migrate, MigrateCommand
+from app.models import User, Role
+
 
 app = create_app("default")
 
@@ -10,7 +12,7 @@ manager = Manager(app)
 migrate = Migrate(app, db)
 
 def make_shell_context():
-    return dict(db=db, app=app)
+    return dict(db=db, app=app, User=User, Role=Role)
 
 manager.add_command("shell", Shell(make_context=make_shell_context))  ##
 manager.add_command("db", MigrateCommand)
